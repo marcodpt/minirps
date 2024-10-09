b6415f4ba526586b7d34465b8d05d1d1ea4d31ff
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-10-09 14:46:02 -0300

    add parse json

--- src/templates/addons/mod.rs
+++ src/templates/addons/mod.rs
@@ -1,8 +1,11 @@
 mod cmd;
+mod parse;
 
 use minijinja::Environment;
 use cmd::cmd;
+use parse::parse;
 
 pub fn addons (env: &mut Environment) {
+    env.add_filter("parse", parse);
     env.add_function("cmd", cmd);
 }
---
+++ src/templates/addons/parse.rs
@@ -0,0 +1,19 @@
+use serde_json;
+use minijinja::{Error, ErrorKind::InvalidOperation, Value};
+
+pub fn parse (value: &str, encoding: &str) -> Result<Value, Error> {
+    if encoding == "json" {
+        match serde_json::from_str::<Value>(value) {
+            Ok(result) => Ok(result),
+            Err(err) => Err(Error::new(
+                InvalidOperation,
+                format!("Fail to parse to {}\n{:#}", encoding, err)
+            ))
+        }
+    } else {
+        Err(Error::new(
+            InvalidOperation,
+            format!("<{}> encoding not implemented!", encoding)
+        ))
+    }
+}
