8273ca73eb1ac10a7d69b1fee8a564f2282889fe
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-10-09 11:39:04 -0300

    Better cmd

--- src/templates/addons/cmd.rs
+++ src/templates/addons/cmd.rs
@@ -1,31 +1,39 @@
+use serde_derive::Serialize;
 use std::process::Command;
 use std::str::from_utf8;
-use minijinja::{Error, ErrorKind::InvalidOperation};
+use minijinja::{Error, ErrorKind::InvalidOperation, Value};
 
-pub fn cmd(cmd: String) -> Result<String, Error> {
+fn parser(data: &Vec<u8>) -> Result<String, Error> {
+    match from_utf8(&data) {
+        Ok(data) => Ok(data.to_string()),
+        Err(err) => Err(Error::new(
+            InvalidOperation,
+            format!("Fail to parse command result!\n{:#}", err)
+        ))
+    }
+}
+
+#[derive(Serialize)]
+struct Output {
+    code: Option<i32>,
+    stdout: String,
+    stderr: String
+}
+
+pub fn cmd(cmd: String) -> Result<Value, Error> {
     let result = match Command::new("sh").arg("-c").arg(&cmd).output() {
         Ok(result) => result,
         Err(err) => {
             return Err(Error::new(
                 InvalidOperation,
-                format!("Fail to execute command:\n{}\n\n{:#}",
-                    &cmd, err
-                ))
-            );
-        }
-    };
-
-    let stdout = match from_utf8(&result.stdout) {
-        Ok(stdout) => stdout,
-        Err(err) => {
-            return Err(Error::new(
-                InvalidOperation,
-                format!("Fail to parse command output:\n{}\n\n{:#}",
-                    &cmd, err
-                )
+                format!("Fail to execute command!\n\n{:#}", err)
             ));
         }
     };
 
-    Ok(stdout.to_string())
+    Ok(Value::from_serialize(Output {
+        code: result.status.code(),
+        stdout: parser(&result.stdout)?,
+        stderr: parser(&result.stderr)?
+    }))
 }
