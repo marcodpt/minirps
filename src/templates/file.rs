072686b916a0b3946172601802c115fa831aa986
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-10-16 10:38:06 -0300

    better file api

--- src/templates/file.rs
+++ src/templates/file.rs
@@ -1,9 +1,8 @@
 use std::fs;
-use std::path::{Path};
-use minijinja::{Error, ErrorKind::InvalidOperation, Value};
-use serde_json::to_string_pretty;
+use std::path::Path;
+use minijinja::Value;
 
-pub fn read (entry: &str) -> Result<Value, Error> {
+pub fn read (entry: &str) -> Option<Value> {
     let path = Path::new(entry);
 
     if path.try_exists().unwrap_or(false) {
@@ -20,99 +19,57 @@ pub fn read (entry: &str) -> Result<Value, Error> {
                             }
                         }
                     }
-                    Ok(files.into())
+                    Some(files.into())
                 },
-                Err(err) => Err(Error::new(
-                    InvalidOperation,
-                    format!("Fail to open directory: {}\n{:#}", entry, err)
-                ))
+                Err(_) => None
             }
         } else {
             match fs::read(path) {
-                Ok(data) => Ok(data.into()),
-                Err(err) => Err(Error::new(
-                    InvalidOperation,
-                    format!("Fail to read file: {}\n{:#}", entry, err)
-                ))
+                Ok(data) => Some(data.into()),
+                Err(_) => None
             }
         }
     } else {
-        Err(Error::new(
-            InvalidOperation,
-            format!("File <{}> does not exist!", entry)
-        ))
+        None
     }
 }
 
-pub fn write (file: &str, data: &Value) -> Result<(), Error> {
+pub fn write (file: &str, data: &Vec<u8>) -> Option<String> {
     let path = Path::new(file);
 
     if let Some(dir) = path.parent() {
         if !dir.exists() {
-            match fs::create_dir_all(dir) {
-                Ok(_) => {},
-                Err(err) => {
-                    return Err(Error::new(
-                        InvalidOperation,
-                        format!("Unable to create dir: {}\n{:#}",
-                            dir.display(), err
-                        )
-                    ));
-                }
-            };
+            if let Err(err) = fs::create_dir_all(dir) {
+                return Some(format!("Unable to create dir: {}\n{:#}",
+                    dir.display(), err
+                ));
+            }
         }
     }
 
-    if let Some(data) = data.as_bytes() {
-        match fs::write(path, data) {
-            Ok(_) => Ok(()),
-            Err(err) => Err(Error::new(
-                InvalidOperation,
-                format!("Unable to write file: {}\n{:#}",
-                    file, err
-                )
-            ))
-        }
-    } else {
-        match to_string_pretty(data.into()) {
-            Ok(data) => match fs::write(path, data) {
-                Ok(_) => Ok(()),
-                Err(err) => Err(Error::new(
-                    InvalidOperation,
-                    format!("Unable to write file: {}\n{:#}",
-                        file, err
-                    )
-                ))
-            },
-            Err(err) => Err(Error::new(
-                InvalidOperation,
-                format!("Unable to format data!\n{:#}", err)
-            ))
-        }
+    match fs::write(path, data) {
+        Ok(_) => None,
+        Err(err) => Some(format!("Unable to write file: {}\n{:#}",
+            file, err
+        ))
     }
 }
 
-pub fn remove (entry: &str) -> Result<(), Error> {
+pub fn remove (entry: &str) -> Option<String> {
     let path = Path::new(entry);
 
     if path.is_dir() {
         match fs::remove_dir_all(path) {
-            Ok(_) => Ok(()),
-            Err(err) => Err(Error::new(
-                InvalidOperation,
-                format!("Unable to remove dir: {}\n{:#}",
-                    entry, err
-                )
+            Ok(_) => None,
+            Err(err) => Some(format!(
+                "Unable to remove dir: {}\n{:#}", entry, err
             ))
         }
     } else {
         match fs::remove_file(path) {
-            Ok(_) => Ok(()),
-            Err(err) => Err(Error::new(
-                InvalidOperation,
-                format!("Unable to remove file: {}\n{:#}",
-                    entry, err
-                )
+            Ok(_) => None,
+            Err(err) => Some(format!(
+                "Unable to remove file: {}\n{:#}", entry, err
             ))
         }
     }
