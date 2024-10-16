55af00a513396c12f7d47100a9ea16e3b6122b76
Marco Di Pillo Tomic <marcodpt@protonmail.com>
2024-10-16 11:06:41 -0300

    better file api

--- src/templates/file.rs
+++ src/templates/file.rs
@@ -1,25 +1,48 @@
 use std::fs;
 use std::path::Path;
 use minijinja::Value;
+use serde_derive::Serialize;
+
+#[derive(Serialize)]
+struct File {
+    is_dir: bool,
+    is_file: bool,
+    is_symlink: bool,
+    name: String,
+    len: u64
+}
 
 pub fn read (entry: &str) -> Option<Value> {
     let path = Path::new(entry);
 
     if path.try_exists().unwrap_or(false) {
         if path.is_dir() {
-            let mut files: Vec<String> = Vec::new();
+            let mut files: Vec<File> = Vec::new();
             match fs::read_dir(path) {
                 Ok(entries) => {
                     for entry in entries {
                         if let Ok(entry) = entry {
-                            if let Some(name) = entry.path().file_name() {
+                            let p = entry.path();
+                            let mut fname = String::new();
+                            if let Some(name) = p.file_name() {
                                 if let Some(name) = name.to_str() {
-                                    files.push(name.to_string());
+                                    fname = name.to_string();
                                 }
                             }
+                            let mut len: u64 = 0;
+                            if let Ok(meta) = p.metadata() {
+                                len = meta.len();
+                            }
+                            files.push(File {
+                                is_dir: p.is_dir(),
+                                is_file: p.is_file(),
+                                is_symlink: p.is_symlink(),
+                                name: fname,
+                                len
+                            });
                         }
                     }
-                    Some(files.into())
+                    Some(Value::from_serialize(files))
                 },
                 Err(_) => None
             }
